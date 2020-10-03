d3ee2e143ff98a9b2f8b01d4a18d761a281fc8c5
Cyrus Mahdavi <mahdavic2@gmail.com>
2020-10-03 19:54:45 +0000

    CLI wip, Wheel torque very WIP to allow variable engine torque points

--- lapsim/lapsim-2020/lapsim_runner.py
+++ lapsim/lapsim-2020/lapsim_runner.py
@@ -11,9 +11,16 @@ parser.read('config.ini')
 torque_points = int(parser.get('engine_torque', 'amt_points'))
 
 #Reads engine torque values from "engine_torque" in config.ini into array "ENGINE_TORQUE"
-for i in range(torque_points):
-    ENGINE_TORQUE.append(float(parser.get('engine_torque', str(i+3) + 'k')))
+engine_info = parser.items('engine_torque')
+for i in engine_info:
+    if i[0][0].isdigit():
+        rpm = int(i[0])
+        torque = float(i[1])
+        ENGINE_TORQUE.append((rpm, torque))
+
 print(ENGINE_TORQUE)
+        
+
 
 for i in parser.items("gear_ratios"):
     if "gear" in i[0]:
@@ -25,15 +32,27 @@ CAR_DIMENSIONS = dict(parser.items("car_dimensions"))
 CAR_WEIGHT = dict(parser.items("car_weight"))
 CAR_DYNAMICS = dict(parser.items("car_dynamics"))
 
-#converts the strings in the dict to floats and ints
-def convert_strings(current_section):
-    for i in current_section: 
-        if "." in current_section[i]:
-            current_section[i] = float(current_section[i])
-        else:
-            current_section[i] = int(current_section[i])
+#Creates int from value in dict
+"""
+WHEEL_BASE = int(parser.get('car_dimensions','wheel_base'))
+FRONT_TRACK = int(parser.get('car_dimensions','front_track'))
+REAR_TRACK = int(parser.get('car_dimensions','rear_track'))
+WEIGHT = int(parser.get('car_weight','weight'))
+WEIGHT_DISTRIBUTION = int(parser.get('car_weight','weight_distribution'))
+HEIGHT_GRAVITY = int(parser.get('car_dynamics','height_gravity'))
+COEFFICENT_DRAG = int(parser.get('car_dy'))
+HEIGHT_CENTER_PRESSURE = int(parser.get('car_dynamics','height_center_pressure'))
+#OCENTER_PRESSURE_REARWARD = int(parser.get('car_dycoefficent_liftmic        if "." in current_section[i]:
+#            current_section[i] = float(current_section[i])
+#        else:
+#            current_section[i] = int(current_section[i])
 
-convert_strings(CAR_DIMENSIONS)
-convert_strings(CAR_WEIGHT)
-convert_strings(CAR_DYNAMICS)
+#convert_strings(CAR_DIMENSIONS)
+#convert_strings(CAR_WEIGHT)
+#convert_strings(CAR_DYNAMICS)
     
+print(CAR_DIMENSIONS)
+#{'wheel_base': 420, 'front_track': 1.1, 'rear_track': 1}
+CAR_DIMENSIONS["torque"] = 15
+print(CAR_DIMENSIONS)
+"""
--- lapsim/lapsim-2020/wheel_torque.py
+++ lapsim/lapsim-2020/wheel_torque.py
@@ -1,12 +1,16 @@
 import math
-from lapsim_runner import ENGINE_TORQUE, GEAR_RATIOS #Imports constants
-def wheel_torque(velocity, gear):
+from lapsim_runner import ENGINE_TORQUE, GEAR_RATIOS#, ENGINE_REDLINE #Imports constants
+def wheel_torque(velocity):
     #WHAT IS "PRIMARY" in f_torque?????
     FINAL_DRIVE = 3.5
     TIRE_RADIUS = 0.853
     PRIMARY = 2.545
 
-
+    #ENGINE_TORQUE = [31,32,32.7,33,33,32.5,31.5,29.5,27]
+    #GEAR_RATIOS = [2.583,1.923,1.533,1.263,1.05]
+    ENGINE_REDLINE = 11000
+    ENGINE_MIN_SPEED = ENGINE_TORQUE[0][0]
+    print(ENGINE_MIN_SPEED)
 
     #RPM = [0] * len(GEAR_RATIOS)#AMT GEARS
 
@@ -18,20 +22,40 @@ def wheel_torque(velocity, gear):
 
     #Using gear and velocity, get rpm and torque then calculate final output to wheel
 
-
-    #
-    RPM = (velocity*60*gear*FINAL_DRIVE * PRIMARY) / (2*math.pi*TIRE_RADIUS)
-    low_rpm_k = math.floor(RPM/1000)
-    print (low_rpm_k)
-
-    cur_engine_torque = 0
+    engine_rpm_per_gear = [0] * len(GEAR_RATIOS)
+    
+    
+    #calculates engine rpm for each gear
+    for i in range(len(GEAR_RATIOS)):
+        engine_rpm_per_gear[i] = (velocity*60*GEAR_RATIOS[i]*FINAL_DRIVE * PRIMARY) / (2*math.pi*TIRE_RADIUS)
+        
+        
+    
+    wheel_torque_per_gear = [0] * len(GEAR_RATIOS)
+    print(engine_rpm_per_gear)
+    for i in range(len(GEAR_RATIOS)):
+        cur_torque_idx = 0
+        if engine_rpm_per_gear[i]<ENGINE_MIN_SPEED or engine_rpm_per_gear[i]>ENGINE_REDLINE:
+            cur_torque_idx = -1
+        else:
+            while cur_torque_idx < len(ENGINE_TORQUE)-1 and ENGINE_TORQUE[cur_torque_idx][0] <= engine_rpm_per_gear[i]:
+                cur_torque_idx+=1
+        print(cur_torque_idx)
+        if cur_torque_idx == -1:
+            wheel_torque_per_gear[i] = 0
+        else:
+            wheel_torque_per_gear[i] = (((engine_rpm_per_gear[i] - ENGINE_TORQUE[i-1][0]) * (ENGINE_TORQUE[i][1] - ENGINE_TORQUE[i-1][1]))/ (ENGINE_TORQUE[i][0] - ENGINE_TORQUE[i-1][0])) + ENGINE_TORQUE[i-1][1]
+        
+    print(wheel_torque_per_gear)
+    
     #NO torque if below idle or above redline
     if low_rpm_k>=3 and low_rpm_k <11:
-        cur_engine_torque = ((RPM - low_rpm_k*1000) * ENGINE_TORQUE[low_rpm_k+1 - 3] - ENGINE_TORQUE[low_rpm_k - 3] )/1000 + ENGINE_TORQUE[low_rpm_k - 3]
+        #cur_engine_torque = ENGINE_TORQUE[low_rpm_k-3]
+        cur_engine_torque = ((cur_engine_rpm - low_rpm_k*1000) * (ENGINE_TORQUE[low_rpm_k+1 - 3] - ENGINE_TORQUE[low_rpm_k - 3]) )/1000 + ENGINE_TORQUE[low_rpm_k - 3]
         print(cur_engine_torque, "cur engine torque")
 
     #Calculate torque at the wheels
-    cur_wheel_torque = (cur_engine_torque*gear*FINAL_DRIVE*PRIMARY) / TIRE_RADIUS    
+    cur_wheel_torque = (cur_engine_torque*GEAR_RATIOS[cur_gear-1]*FINAL_DRIVE*PRIMARY) / TIRE_RADIUS    
 
     print(cur_wheel_torque, "wheel torque")
 
@@ -41,4 +65,4 @@ def wheel_torque(velocity, gear):
         #Get 2 surrounding torque figures for each rmp.
         """
 
-wheel_torque(60, 1.533)
+wheel_torque(60)
